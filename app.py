49806d839f19bee5f16034f8ea9d93c4cfd427aa
Zach Manson <zachpmanson@gmail.com>
2022-05-02 13:47:06 +0800

    Add minecraft style buttons and howto play page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from app import app 
 
-if __init__ == "__main__":
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    render_args = {
+        "title":"Minecrafdle"
+    }
     return render_template(
         'index.html',
-        title="Minecrafdle"
+        **render_args
+    )
+
+@app.route('/how-to-play')
+def rules():
+    render_args = {
+        "title":"How To Play"        
+    }
+    return render_template(
+        'how-to-play.html',
+        **render_args
     )
