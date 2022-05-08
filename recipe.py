8224f758d891ec69cb5fafc9475c3f1f4196a1ba
Oatesha <73292759+Oatesha@users.noreply.github.com>
2022-05-09 03:22:10 +0800

    Finished script

--- recipe.py
+++ recipe.py
@@ -1,56 +1,91 @@
-from ast import pattern
 import json, os
-from multiprocessing.sharedctypes import Value
-from re import L
-recipesprocessed = []
+from tkinter import N
 
-path = '../recipes/'
-recipes = [recipe for recipe in os.listdir(path) if recipe.endswith('.json')]
-for i in recipes:
-    jsonfile = open(path + i)
-    recipesprocessed.append(json.load(jsonfile))
+def writejson(dictionary, filename):
+    jsonobject = json.dumps(dictionary)
+    with open(("../sanitised recipes/" + filename), 'w') as jsonfile:
+        jsonfile.write(jsonobject)
     
-for i in recipesprocessed:
-    standard_dict = {
-            "type": "",
-            "group": "",
-            "output": "",
-            "input": [
-                    [None, None, None],
-                    [None, None, None],
-                    [None, None, None]
-                ]
-            }
-    for key, value in i.items():
-        if key == "type":
-            standard_dict["type"] = value
-            
-        if key == "group":
-            standard_dict["group"] = value
-            
-        if key == "pattern":
-            for listindex, desiredpattern in enumerate(value):
-                for desiredindex, desiredpattern in enumerate(desiredpattern):
-                    standard_dict["input"][listindex][desiredindex] = desiredpattern
+
+
+def processrecipes(path):
+    recipesprocessed = []
+
+    recipes = [recipe for recipe in os.listdir(path) if recipe.endswith('.json')]
+    for i in recipes:
+        jsonfile = open(path + i)
+        recipesprocessed.append(json.load(jsonfile))
         
-        if key == "key":
-            for nestedkey, nestedvalue in value.items():
-                for i in  standard_dict["input"]:
-                    for x in i:
-                        if x == nestedkey:
-                            if isinstance(nestedvalue, dict):
-                                for firstlist in range(len(standard_dict["input"])):
-                                    for secondlist in range(len(standard_dict["input"][firstlist])):
-                                        if standard_dict["input"][firstlist][secondlist] == nestedkey:
-                                            standard_dict["input"][firstlist][secondlist] = 'None'.join(list(nestedvalue.values()))
+    for jsonfile, name in zip(recipesprocessed, recipes):
+        standard_dict = {
+                "type": "",
+                "group": "",
+                "output": "",
+                "input": [
+                        [None, None, None],
+                        [None, None, None],
+                        [None, None, None]
+                    ]
+                }
+        for key, value in jsonfile.items():
+            if key == "type":
+                standard_dict["type"] = value
+                
+            if key == "group":
+                standard_dict["group"] = value
+                
+                
+            if key == "pattern":
+                for listindex, desiredpattern in enumerate(value):
+                    for desiredindex, desiredpattern in enumerate(desiredpattern):
+                        if desiredpattern != ' ':
+                            standard_dict["input"][listindex][desiredindex] = desiredpattern
+            
+            if key == "key":
+                for nestedkey, nestedvalue in value.items():
+                    for i in  standard_dict["input"]:
+                        for x in i:
+                            if x == nestedkey:
+                                if isinstance(nestedvalue, dict):
+                                    for firstlist in range(len(standard_dict["input"])):
+                                        for secondlist in range(len(standard_dict["input"][firstlist])):
+                                            if standard_dict["input"][firstlist][secondlist] == nestedkey:
+                                                standard_dict["input"][firstlist][secondlist] = 'None'.join(list(nestedvalue.values()))
+                                else:
+                                    for firstlist in range(len(standard_dict["input"])):
+                                        for secondlist in range(len(standard_dict["input"][firstlist])):
+                                            if standard_dict["input"][firstlist][secondlist] == nestedkey:
+                                                standard_dict["input"][firstlist][secondlist] = 'None'.join(list(nestedvalue[0].values()))
+            if key == "ingredients":
+                if isinstance(value, list):
+                    del standard_dict["input"]
+                    for i in value:
+                        if isinstance(i, dict):
+                            for ingredientkey in i.keys():
+                                if "input" in standard_dict:
+                                    standard_dict["input"].append(i[ingredientkey])
+                                    
+                                else:
+                                    standard_dict["input"] = list(i[ingredientkey].split())
+                        else:
+                            if "input" in standard_dict:
+                                standard_dict["input"].append(i)
+    
                             else:
-                                for firstlist in range(len(standard_dict["input"])):
-                                    for secondlist in range(len(standard_dict["input"][firstlist])):
-                                        if standard_dict["input"][firstlist][secondlist] == nestedkey:
-                                            standard_dict["input"][firstlist][secondlist] = 'None'.join(list(nestedvalue[0].values()))
-        if key == "result":
-            if isinstance(value, dict):
-                standard_dict["output"] = value["item"]
-            else:
-                standard_dict["output"] = value
-    print(standard_dict)
+                                standard_dict["input"] = list(i)
+
+
+            
+            if key == "result":
+                if isinstance(value, dict):
+                    standard_dict["output"] = value["item"]
+                else:
+                    standard_dict["output"] = value
+        writejson(standard_dict, name)
+
+    
+def main():
+    processrecipes("../recipes/")
+
+if __name__ == "__main__":
+    main()
