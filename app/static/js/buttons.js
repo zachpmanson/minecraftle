b2ada263b0c026fa8613a302e7e71c6f15c373ab
pavo-etc <z1verdeman2@gmail.com>
2022-05-18 17:15:18 +0800

    Streamline audio triggers

---
+++ app/static/js/buttons.js
@@ -0,0 +1,7 @@
+function triggerAudioButton(href, sound) {
+    var audio = document.getElementById(sound);
+    audio.play();
+    audio.addEventListener('ended', function () {
+      location.href = href;
+    })
+  }
