392982fc2cd9ac598593af294f654b2f4899f329
pavo-etc <z1verdeman2@gmail.com>
2022-05-23 11:12:20 +0800

    Add jsdoc

--- app/static/js/buttons.js
+++ app/static/js/buttons.js
@@ -1,3 +1,9 @@
+
+/**
+ * Plays audio then clicks button
+ * @param {string} href 
+ * @param {string} sound 
+ */
 function triggerAudioButton(href, sound) {
     var audio = document.getElementById(sound);
     audio.play();
@@ -6,6 +12,9 @@ function triggerAudioButton(href, sound) {
     });
 }
 
+/**
+ * Plays music, done here to avoid 8MB file transfer on pageload
+ */
 function playMusic() {
   var audio = new Audio('/static/audio/C418 - Aria Math (Minecraft Volume Beta).mp3');
   audio.play();
