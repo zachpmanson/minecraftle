9ee79e33374992687ae5299ee7cb177ff68d6477
Tamura77 <tnbddd77@gmail.com>
2022-05-23 11:33:12 +0800

    Comments to all stats.js

--- app/static/js/stats.js
+++ app/static/js/stats.js
@@ -1,8 +1,11 @@
 //Processing Social media buttons
 let shareable_link = String(document.location).split("&")[0];
 
-//Tweet
-function getT(text, url) {
+/**
+ * Generates twitter post link structure with pre build tweet message.
+ * @returns {String} A shareable twitter link to be appended to a popup tab
+ */
+function getT() {
   return (
     "https://twitter.com/intent/tweet?text=" +
     "Check out my latest Minecraftle Stats:" +
@@ -11,40 +14,63 @@ function getT(text, url) {
   );
 }
 
+/**
+ * Collects twitter post link structure and calls a popup window function to post it
+ */
 function tweet() {
   var url = getT();
   PopupCenter(url, "Share to Twitter", "590", "253");
 }
 
-//Facebook
-function getFB(text, url) {
+/**
+ * Generates facebook post link structure with pre build post message.
+ * @returns {String} A shareable facebook link to be appended to a popup tab
+ */
+function getFB() {
   return (
     "https://www.facebook.com/sharer.php?u=" +
     shareable_link +
-    "Check out my stats"
+    "Check out my latest Minecraftle Stats:"
   );
 }
 
+/**
+ * Collects facebook post link structure and calls a popup window function to post it 
+ */
 function facebook() {
   var url = getFB();
   PopupCenter(url, "Share to Facebook", "590", "253");
 }
 
-//Reddit
-function getR(text, url) {
+/**
+ * Generates reddit post link structure with pre build post message.
+ * @returns {text} A shareable reddit link to be appended to a popup tab
+ */
+function getR() {
   return (
     "https://reddit.com/submit?url=" +
     shareable_link +
     "&title=" +
-    "Check out my latest Minecraftle stats"
+    "Check out my latest Minecraftle Stats:"
   );
 }
 
+/**
+ * Collects reddit post link structure and calls a popup window function to post it 
+ */
 function reddit() {
   var url = getR();
   PopupCenter(url, "Share to Reddit", "590", "253");
 }
 
+/**
+ * Constructs a new popup tab in center of the users screen to with input links to automatically post messages
+ * to social media.
+ * @param {String} url 
+ * @param {String} title 
+ * @param {int} w 
+ * @param {int} h 
+ */
 function PopupCenter(url, title, w, h) {
   var dualScreenLeft =
     window.screenLeft != undefined ? window.screenLeft : screen.left;
@@ -77,7 +103,6 @@ function PopupCenter(url, title, w, h) {
       left
   );
 
-  // Puts focus on the newWindow
   if (window.focus) {
     newWindow.focus();
   }
