4835ee6f8222d0dcf5b2864c0df9105cdde1c82f
Zach Manson <zachpmanson@gmail.com>
2025-06-01 08:46:45 +1000

    feat: update cache version

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ export const HICONTRAST_COLORS = {
   SUCCESS_COLOR: "#f5793a",
   NEAR_SUCCESS_COLOR: "#85c0f9",
 };
-export const CACHE_VERSION = "20231222_2";
+export const CACHE_VERSION = "20250531_1";
 
 export const DEFAULT_OPTIONS = {
   highContrast: false,
